a383944a8054cc5e9ed02961dbebe2f3d436307e
jeyofdev <jgregoire.pro@gmail.com>
2022-06-14 15:39:07 +0200

    feat : init appolo-client

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import reportWebVitals from './reportWebVitals';
+import App from './App';
+import './index.css';
+
+const client = new ApolloClient({
+    uri: '/graphql',
+    cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,20 @@
+import { useQuery } from '@apollo/client';
+import { GET_ALL_PROJECTS } from '../queries/query';
+
 const Home = () => {
-  return (
-    <div className="flex justify-center items-center w-full h-full ">
-      {" "}
-      <p className="text-6xl">Home Page</p>
-    </div>
-  );
+    const { loading, error, data } = useQuery(GET_ALL_PROJECTS);
+
+    // if(loading) {
+    //   return "...loading"
+    // }
+
+    console.log(data);
+    return (
+        <div className="flex justify-center items-center w-full h-full ">
+            {' '}
+            <p className="text-6xl">Home Page</p>
+        </div>
+    );
 };
 
 export default Home;
