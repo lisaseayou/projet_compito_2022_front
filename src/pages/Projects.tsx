726e4a0c51e59c868fe8b9ad14f5ef1b2865ba22
lisaseayou <lisa.machy@hotmail.fr>
2022-06-17 17:21:25 +0200

    maj

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import CardsListProject from "../components/CardsListProject";
+import CardsListProject from "../components/Projects/CardsListProject";
 
 const Projects = () => {
   return (
