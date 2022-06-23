09c873f369e335bd23ff30749067580d7e5c1ce3
DavidVM19 <davidvm1910@gmail.com>
2022-06-23 11:31:19 +0200

    fix btn edit & clean task

---
+++ src/components/TableTask.tsx
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Modal from "./Modal";
+import { DotsHorizontalIcon } from "@heroicons/react/solid";
+
+const TableTask = ({
+  task,
+  openModal,
+  setOpenModal,
+}: {
+  task: any;
+  openModal?: number;
+  setOpenModal: (id: number) => void;
+}) => {
+  useEffect(() => {
+    console.log("render");
+  }, [openModal]);
+
+  const showModal = (id: number) => {
+    setOpenModal(id);
+  };
+
+  const hideModal: () => void = () => {
+    setOpenModal(0);
+  };
+  return (
+    <tbody key={task.id}>
+      <tr>
+        <td className="px-5 py-5 border-b border-gray-200 bg-white text-center text-sm w-64">
+          {task.id}
+        </td>
+        <td className="px-5 py-5 border-b border-gray-200 bg-white text-center text-sm w-64">
+          {task.name}
+        </td>
+        <td className="px-5 py-5 border-b border-gray-200 bg-white text-center text-sm w-64">
+          {task.subject}
+        </td>
+        <td className="px-5 py-5 border-b border-gray-200 bg-white text-center text-sm w-64">
+          {task.priority}
+        </td>
+        <td className="px-5 py-5 border-b border-gray-200 bg-white text-center text-sm w-64">
+          {task.status === true ? "Open" : "Closed"}
+        </td>
+        <td className="px-5 py-5 border-b border-gray-200 bg-white text-center text-sm w-64">
+          {task.createDate}
+        </td>
+        <td className="px-5 py-5 border-b border-gray-200 bg-white text-center text-sm w-64">
+          {task.dueDate}
+        </td>
+        <td className="border-b border-gray-200 bg-white text-center text-sm w-64">
+          {task.id === openModal ? (
+            <Modal>
+              <Link to={`/editTask/:${task.id}`}>Edit</Link>
+              {console.log(openModal)}
+              <div>
+                <button type="button" onClick={hideModal}>
+                  Close
+                </button>
+              </div>
+            </Modal>
+          ) : (
+            <button type="button" onClick={() => showModal(task.id)}>
+              <DotsHorizontalIcon className="w-4" />
+            </button>
+          )}
+        </td>
+      </tr>
+    </tbody>
+  );
+};
+
+export default TableTask;
--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,25 +1,17 @@
-import { Link } from "react-router-dom";
 import { useState } from "react";
-import DataTasks from "../data/DataTasks";
-import Modal from "../components/Modal";
 import CardTask from "../components/CardTask";
+import TableTask from "../components/TableTask";
+import DataTasks from "../data/DataTasks";
 
 const Tasks = () => {
+  const [handleDisplay, setHandleDisplay] = useState<boolean>(false);
   const [openModal, setOpenModal] = useState<number>();
-  const [isChecked, setIsChecked] = useState(false);
-
-  const showModal = (id: number) => {
-    setOpenModal(id);
-  };
-  const hideModal: () => void = () => {
-    setOpenModal(0);
-  };
 
   const showCards = (e: any): void => {
     if (e.target) {
-      setIsChecked(!isChecked);
+      setHandleDisplay(!handleDisplay);
     } else {
-      setIsChecked(false);
+      setHandleDisplay(false);
     }
   };
 
@@ -33,97 +25,62 @@ const Tasks = () => {
         className="ml-3 mr-3 mb-4 bg-gray-100 border border-gray-100 p-1 rounded-lg"
         onClick={showCards}
       >
-        {isChecked ? "Afficher en tableau" : "Afficher en card"}
+        {handleDisplay ? "Afficher en tableau" : "Afficher en card"}
       </button>
-      {isChecked ? (
+      {handleDisplay ? (
         <div className="flex flex-col justify-center items-center w-full h-full ">
-          <div className="w-full flex justify-start pl-24 ">
+          <div className="w-full flex justify-start pl-10 ">
             <div className="flex">
-              {DataTasks.map((task, index) => (
-                <CardTask key={index} task={task} />
+              {DataTasks.map((task) => (
+                <CardTask
+                  key={task.id}
+                  task={task}
+                  openModal={openModal}
+                  setOpenModal={setOpenModal}
+                />
               ))}
             </div>
           </div>
         </div>
       ) : (
         <div>
-          <table className="m-4  leading-normal font-poppins">
+          <table className="m-4 leading-normal ">
             <thead>
               <tr>
-                <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                <th className="px-5 py-3 border-b-2 border-gray-200 text-center text-xs font-bold text-blue-600 tracking-wider">
                   ID
                 </th>
-                <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                <th className="px-5 py-3 border-b-2 border-gray-200 text-center text-xs font-bold text-blue-600 tracking-wider">
                   Name
                 </th>
-                <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                <th className="px-5 py-3 border-b-2 border-gray-200 text-center text-xs font-bold text-blue-600 tracking-wider">
                   Subject
                 </th>
-                <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                <th className="px-5 py-3 border-b-2 border-gray-200 text-center text-xs font-bold text-blue-600 tracking-wider">
                   Priority
                 </th>
-                <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                <th className="px-5 py-3 border-b-2 border-gray-200 text-center text-xs font-bold text-blue-600 tracking-wider">
                   Status
                 </th>
-                <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                <th className="px-5 py-3 border-b-2 border-gray-200 text-center text-xs font-bold text-blue-600 tracking-wider">
                   Create Date
                 </th>
-                <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                <th className="px-5 py-3 border-b-2 border-gray-200 text-center text-xs font-bold text-blue-600 tracking-wider">
                   Due Date
                 </th>
-                <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                <th className="px-5 py-3 border-b-2 border-gray-200 text-center text-xs font-bold text-blue-600 tracking-wider">
                   Action
                 </th>
               </tr>
             </thead>
-            <tbody>
-              {DataTasks.map((data) => {
-                return (
-                  <tr key={data.id}>
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-center text-sm w-64">
-                      {data.id}
-                    </td>
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-center text-sm w-64">
-                      {data.name}
-                    </td>
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-center text-sm w-64">
-                      {data.subject}
-                    </td>
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-center text-sm w-64">
-                      {data.priority}
-                    </td>
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-center text-sm w-64">
-                      {data.status === true ? "Open" : "Closed"}
-                    </td>
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-center text-sm w-64">
-                      {data.createDate}
-                    </td>
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-center text-sm w-64">
-                      {data.dueDate}
-                    </td>
-                    <td className="border-b border-gray-200 bg-white text-center text-sm w-64">
-                      {data.id === openModal ? (
-                        <Modal openModal={openModal}>
-                          <Link to={`/editTask/:${data.id}`}>Edit</Link>
-                          <div>
-                            <button type="button" onClick={hideModal}>
-                              Close
-                            </button>
-                          </div>
-                        </Modal>
-                      ) : (
-                        <button
-                          type="button"
-                          onClick={() => showModal(data.id)}
-                        >
-                          .....
-                        </button>
-                      )}
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
+            {DataTasks.map((task) => (
+              <TableTask
+                key={task.id}
+                task={task}
+                openModal={openModal}
+                setOpenModal={setOpenModal}
+              />
+            ))}
           </table>
         </div>
       )}
