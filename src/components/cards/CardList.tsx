2ad1ddf2bb3269b39c3dbbe3bd4b9af9aab75716
jeyofdev <jgregoire.pro@gmail.com>
2022-09-02 09:53:18 +0200

    feat - list cards tasks for user/home

---
+++ src/components/cards/CardList.tsx
@@ -0,0 +1,34 @@
+import { ReactNode } from 'react';
+import Typography, { variantEnum } from '../ui/Typography';
+
+type CardListProps = {
+    title: string;
+    icon: any;
+    children: ReactNode;
+};
+const CardList = ({ title, icon, children }: CardListProps) => {
+    return (
+        <div className="flex justify-start bg-white border-b border-primary-main border-opacity-40 py-5">
+            <div className="flex justify-center items-center h-8 w-8 mr-2 bg-primary-main">
+                {icon}
+            </div>
+
+            <div className="flex justify-between items-center w-full">
+                <div className="flex">
+                    <Typography
+                        variant={variantEnum?.H5}
+                        color="text-primary-main"
+                        fontSize="text-1xl"
+                        fontWeight="font-semi-bold"
+                        className="w-full"
+                    >
+                        {title}
+                    </Typography>
+                </div>
+                {children}
+            </div>
+        </div>
+    );
+};
+
+export default CardList;
--- src/pages/user/UserHome.tsx
+++ src/pages/user/UserHome.tsx
@@ -1,10 +1,18 @@
 import { useQuery } from '@apollo/client';
-import { ClockIcon } from '@heroicons/react/solid';
+import {
+    ColorSwatchIcon,
+    ClockIcon,
+    ClipboardCheckIcon,
+} from '@heroicons/react/solid';
+import { ClockIcon as ClockIconOutline } from '@heroicons/react/outline';
 import { GET_ALL_PROJECTS } from '../../queries/query';
 import HeaderGlobal from '../../components/headers/HeaderGlobal';
 import TitleBlock from '../../components/titles/TitleBlock';
 import CardSmall from '../../components/cards/CardSmall';
 import CardAddSmall from '../../components/cards/CardAddSmall';
+import Typography, { variantEnum } from '../../components/ui/Typography';
+import Work from '../../assets/work-pressure.svg';
+import CardList from '../../components/cards/CardList';
 
 const UserHome = () => {
     const { loading, error, data } = useQuery(GET_ALL_PROJECTS);
@@ -29,7 +37,7 @@ const UserHome = () => {
                         <TitleBlock
                             title="Récemment consultés"
                             icon={
-                                <ClockIcon className="h-5 w-5 text-primary-main mr-4" />
+                                <ClipboardCheckIcon className="h-5 w-5 text-primary-main mr-4" />
                             }
                         />
 
@@ -47,6 +55,54 @@ const UserHome = () => {
                             </div>
                         </div>
                     </div>
+
+                    <div className="row-start-2 col-span-12 md:col-span-4">
+                        <div className="flex flex-col mt-8">
+                            <TitleBlock
+                                title="Taches pour aujourd'hui"
+                                icon={
+                                    <ClockIcon className="h-5 w-5 text-primary-main mr-4" />
+                                }
+                            />
+                        </div>
+
+                        <hr className="col-start-1 col-end-13 h-1 w-24 bg-primary-main mt-2 mb-4" />
+
+                        <div className="flex flex-col">
+                            {data.allProjects
+                                .slice(0, 4)
+                                .map((project: any) => (
+                                    <CardList
+                                        title={project?.name}
+                                        icon={
+                                            <ColorSwatchIcon className="h-6 w-6 text-white" />
+                                        }
+                                    >
+                                        <div className="flex items-center">
+                                            <ClockIconOutline className="h-6 w-6 text-primary-main mr-1" />
+                                            <Typography
+                                                variant={variantEnum?.H6}
+                                                color="text-primary-main"
+                                                fontSize="text-xs"
+                                                fontWeight="font-bold"
+                                                className="w-full"
+                                            >
+                                                3h
+                                            </Typography>
+                                        </div>
+                                    </CardList>
+                                ))}
+                        </div>
+                    </div>
+
+                    <div
+                        className="row-start-3 md:row-start-2 col-span-12 md:col-span-4 mt-12"
+                        style={{
+                            background: `url(${Work}) no-repeat`,
+                            backgroundSize: 'cover',
+                            backgroundPosition: 'center',
+                        }}
+                    />
                 </div>
             </div>
         </section>
