c09320e8b581d2ab6ec9a812ef90268379846cde
jeyofdev <jgregoire.pro@gmail.com>
2022-10-07 13:12:52 +0200

    fix : fix 404

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,31 +1,34 @@
 import Button from '../components/ui/Buttons/Button';
 import { ButtonTypeEnum, ButtonVariantEnum } from '../enums';
+import notFoundImage from '../assets/404.svg';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
         <div className="flex h-screen">
             <div className="m-auto grid grid-cols-1 sm:grid-cols-1 xl:grid-cols-2 p-10">
                 <div>
-                    <img src="./assets/images/404.svg" alt="404-error" />
+                    <img src={notFoundImage} alt="404-error" />
                 </div>
                 <div className="flex flex-col justify-center items-center">
                     <p className="text-6xl font-bold uppercase text-sky-600">
-                        Oh no !!!
+                        Oh non !!!
                     </p>
                     <p className="text-6xl font-bold uppercase text-sky-600">
                         Error 404
                     </p>
                     <p className="mt-10 text-xl text-sky-500">
-                        we can't seem to find the page your looking for
+                        Nous n'arrivons pas à trouver la page que vous
+                        recherchez.
                     </p>
-                    <a className="mt-10" href="/">
+                    <Link className="mt-10" to="/user/home">
                         <Button
                             type={ButtonTypeEnum?.SUBMIT}
                             variant={ButtonVariantEnum.PRIMARY}
                         >
                             Go to home page
                         </Button>
-                    </a>
+                    </Link>
                 </div>
             </div>
         </div>
